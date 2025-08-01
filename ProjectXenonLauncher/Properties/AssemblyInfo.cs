23c55118f1e81c7be0713c7c135658ceba060a81
sharklebanan <114457867+sharklebanan@users.noreply.github.com>
2025-08-01 23:02:44 +1000

    Properties
    amazing description. please make better ones moving forward!! thanks
    
    ~ Miss Sharkle Banan

---
+++ ProjectXenonLauncher/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Resources;
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ProjectXenonLauncher")]
+[assembly: AssemblyDescription("Launcher for Project Xenon, a Roblox revival")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Team Xenon")]
+[assembly: AssemblyProduct("ProjectXenonLauncher")]
+[assembly: AssemblyCopyright("2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("4.1.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
